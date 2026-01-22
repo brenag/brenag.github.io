f2b69c6e4b47d18cf673bdb6e761035b88e09057
brenag <51867736+brenag@users.noreply.github.com>
2026-01-22 15:41:27 +0000

    Deploying to gh-pages from @ brenag/brenag.github.io@394b5b5e9279b2b865f55f055ee02ad3d32d00b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -40,7 +40,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
